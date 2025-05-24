f5fd8175fc94233920b1a445dc256e76b8f3b043
Emirchelo-Lopez <elopezarmenincome@gmail.com>
2025-05-23 21:49:46 -0600

    create mi-exploracion-espacial project for lesson 7

---
+++ lesson-7/mi-exploracion-espacial/planetas.js
@@ -0,0 +1,36 @@
+const planetas = [
+  {
+    nombre: "Titán",
+    descripcion: "La luna más grande de Saturno, con lagos de metano.",
+    descubiertoEn: "1655"
+  },
+  {
+    nombre: "Próxima Centauri b",
+    descripcion: "Un exoplaneta rocoso en la zona habitable de su estrella.",
+    descubiertoEn: "2016"
+  },
+  {
+    nombre: "Kepler-186f",
+    descripcion: "Un exoplaneta en la zona habitable de su estrella.",
+    descubiertoEn: "2014"
+  },
+  {
+    nombre: "Gliese 581g",
+    descripcion: "Un exoplaneta potencialmente habitable.",
+    descubiertoEn: "2010"
+  },
+  {
+    nombre: "Luna",
+    descripcion: "La luna de la Tierra, nuestro satélite natural.",
+    descubiertoEn: "Prehistoria"
+  },
+  {
+    nombre: "Marte",
+    descripcion: "El planeta rojo, conocido por sus características geológicas.",
+    descubiertoEn: "Antigüedad"
+  }
+  // ¡Añade más planetas!
+];
+
+module.exports = planetas;
+// ¡No olvides exportar la variable planetas!
